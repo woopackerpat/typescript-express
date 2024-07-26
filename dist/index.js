50ab1f8a9e4ca28b5ef399df2f62711678fb52cb
woopackerpat <p.taechakraichana@gmail.com>
2024-07-26 16:20:25 +0700

    remove prod i

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)({ path: path_1.default.join(__dirname, "../.env") });
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const error_1 = __importDefault(require("./middlewares/error"));
